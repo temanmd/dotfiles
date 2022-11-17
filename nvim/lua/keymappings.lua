93f71e4bc598aee42c6be7dfd4df1135c580ebae
Artem Belankov <temanmd@gmail.com>
2022-11-17 12:37:36 +0600

    Add some shortcuts

--- nvim/lua/keymappings.lua
+++ nvim/lua/keymappings.lua
@@ -33,3 +33,15 @@ bind("n", "gp", ":bp<CR>", opts)
 
 bind('n', "<leader>b", "<cmd>lua require('telescope.builtin').buffers()<cr>", opts)
 
+-- Telescope
+-- keymap("n", "gi", "<cmd>lua require('telescope.builtin').lsp_implementations()<cr>", opts)
+-- keymap("n", "gd", "<cmd>lua require('telescope.builtin').lsp_definitions()<cr>", opts)
+bind("n", "<leader>sf", "<cmd>lua require('telescope.builtin').find_files()<cr>", opts)
+bind("n", "<leader>sb", "<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<cr>", opts)
+bind("n", "<leader>sg", "<cmd>lua require('telescope.builtin').live_grep()<cr>", opts)
+bind("n", "<leader>sd", "<cmd>lua require('telescope.builtin').diagnostics()<cr>", opts)
+bind("n", "<leader>sc", "<cmd>lua require('telescope.builtin').git_commits()<cr>", opts)
+bind("n", "<leader>sr", "<cmd>lua require('telescope.builtin').lsp_references()<cr>", opts)
+bind("n", "<leader>so", "<cmd>lua require('telescope.builtin').lsp_document_symbols()<cr>", opts)
+bind("n", "<leader>sh", "<cmd>lua require('telescope.builtin').help_tags()<cr>", opts)
+
