664b156547a569d4ed4bd838dd4acb266e23a357
temanmd <temacxz@gmail.com>
2024-12-30 21:43:27 +0500

    Add emacs config, gopls to zed

---
+++ emacs/init.el
@@ -0,0 +1,13 @@
+(setq inhibit-startup-message t)
+(setq mac-command-modifier 'meta)
+
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+(set-frame-font "JetBrains Mono Medium 15" nil t)
+(blink-cursor-mode 0)
+
+(setq frame-resize-pixelwise window-system)
+
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 printf "\nIt will replace your:\n"
 printf "~/.config/nvim\n"
+printf "~/.emacs.d\n"
 printf "~/.config/tmux/tmux.conf\n"
 printf "~/.config/zed/keymap.json\n"
 printf "~/.config/zed/settings.json\n"
@@ -9,6 +10,9 @@ read -p "Are you sure? (y/n): " confirm && [[ $confirm == [yY] || $confirm == [y
 
 rm -rf ~/.config/nvim || exit 0
 ln -snf $(PWD)/nvim ~/.config/nvim
+
+rm -rf ~/.emacs.d || exit 0
+ln -snf $(PWD)/emacs ~/.emacs.d
 
 ln -snf $(PWD)/tmux/tmux.conf ~/.config/tmux/tmux.conf
 ln -snf $(PWD)/oh-my-zsh/af-magic.zsh-theme ~/.oh-my-zsh/custom/themes/af-magic.zsh-theme
